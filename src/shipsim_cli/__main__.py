78393b8afebe4f13f98677a9eaae3dcf62525c94
espehon <gregg.howlett@gmail.com>
2025-06-27 17:05:01 -0700

    basic file structure

--- src/shipsim_cli/__main__.py
+++ src/shipsim_cli/__main__.py
@@ -2,3 +2,9 @@
 # Copyright (C) 2025 espehon
 # Licensed under the GNU General Public License v3.0
 # See <https://www.gnu.org/licenses/gpl-3.0.html> for details.
+
+import sys
+from shipsim import cli
+
+if __name__ == "__main__":
+    sys.exit(cli())
--- src/shipsim_cli/shipsim.py
+++ src/shipsim_cli/shipsim.py
@@ -4,3 +4,35 @@
 # See <https://www.gnu.org/licenses/gpl-3.0.html> for details.
 
 
+#region Imports
+import sys
+import os
+
+
+
+#endregion Imports
+#region Setup
+
+
+
+
+#endregion Setup
+#region Functions
+
+
+
+
+
+def cli():
+    pass
+
+#endregion Functions
+#region Main
+
+if __name__ == "__main__":
+    print("This program file should not be run directly.\nPlease use run __main__.py or the shipsim command instead.")
+    sys.exit(1)
+
+
+
+#endregion Main
