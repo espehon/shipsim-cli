f280b5fd2554b0fab30d48f878703c754168fc4e
espehon <gregg.howlett@gmail.com>
2025-06-28 20:15:49 -0700

    started setup and args

--- src/shipsim_cli/shipsim.py
+++ src/shipsim_cli/shipsim.py
@@ -7,11 +7,58 @@
 #region Imports
 import sys
 import os
+import argparse
+import json
+import importlib.metadata
+
+
+import pandas as pd
+import questionary
 
 
 
 #endregion Imports
 #region Setup
+
+
+# Set settings file
+settings_file = os.path.expanduser("~/.config/shipsim/settings.json").replace("\\", "/")
+if os.path.exists(settings_file):
+    with open(settings_file, "r") as f:
+        settings = json.load(f)
+else:
+    settings = {
+        "carriers_folder": "~/.local/share/shipsim"
+    }
+    os.makedirs(os.path.dirname(settings_file), exist_ok=True)
+    with open(settings_file, "w") as f:
+        json.dump(settings, f, indent=4)
+        print(f"Created settings file found at {settings_file}")
+
+# Set carriers folder
+if os.path.exists(settings["carriers_folder"]) == False:
+    os.makedirs(settings["carriers_folder"])
+    print(f"Created carriers folder at {settings['carriers_folder']}")
+
+if os.listdir(settings["carriers_folder"]) == False:
+    print("No carriers found in the carriers folder. Please add some carriers to the following:")
+    print(settings["carriers_folder"])
+    print("\nTry 'shipsim --help' for more info.")
+    sys.exit(0)
+
+
+
+# Set Argparse
+parser = argparse.ArgumentParser(
+    description="shipsim-cli: TODO.",
+    epilog="TODO",
+    allow_abbrev=False,
+    add_help=False,
+    usage="shipsim <FromZip> <ToZip> <PkgWeight1> [<PkgWeight2> ...]",
+)
+
+parser.add_argument('-?', '--help', action='help', help='Show this help message and exit.')
+parser.add_argument('shipment_info', nargs=argparse.REMAINDER, help='<FromZip> <ToZip> <PkgWeight>')
 
 
 
@@ -20,18 +67,50 @@ import os
 #region Functions
 
 
+def folder_sys_help():
+    print(f"Please set up your carriers in {settings['carriers_folder']}.")
+    print("Each carrier should be a folder named after the carrier.")
+    print("Inside each carrier folder, there should be a ZoneMap.csv and a RateCard.csv file.")
+    print("Optionally, you can also add Misc.json for additional information like accessorials.")
+    print("""\nExample:
+    shipsim/
+        ├── UPS/
+        │   ├── ZoneMap.csv
+        │   ├── RateCard.csv
+        │   └── Misc.json
+        └── FedEx/
+            ├── ZoneMap.csv
+            └── RateCard.csv\n""")
+
+
+def get_carriers() -> list:
+    """
+    Get a list of carriers from the carriers folder.
+    Returns:
+        list: List of carrier names.
+    """
+    carriers = []
+    for item in os.listdir(settings["carriers_folder"]):
+        if os.path.isdir(os.path.join(settings["carriers_folder"], item)):
+            carriers.append(item)
+    return carriers
 
 
 
-def cli():
-    pass
+
+
+def cli(argv=None):
+    "shipsim 90001 10036 19.69"
+    args = parser.parse_args(argv)
+
+
 
 #endregion Functions
 #region Main
 
 if __name__ == "__main__":
-    print("This program file should not be run directly.\nPlease use run __main__.py or the shipsim command instead.")
-    sys.exit(1)
+    print("This program file should not be run directly.\nPlease run __main__.py or the use the shipsim command instead.")
+    sys.exit(0)
 
 
 
